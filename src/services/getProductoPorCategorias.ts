5174e3e96c2c9681ece12bb6e3583242a14893ab
Maria Elena Miranda <ifp.mara@gmail.com>
2025-11-22 17:20:10 +0100

    Realización de getProductosPorCategorias

---
+++ src/services/getProductoPorCategorias.ts
@@ -0,0 +1,17 @@
+import { Producto } from "../domain/entities/producto";
+import { productosDB } from "../data/productosDb";
+
+//Función para obtener un array de productos de la misma categoria a través de una promesa
+export async function getProductosPorCategoria(categoria : string) : Promise<Producto[]> {
+  const promesa : Promise <Producto[]> = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      const productos : Producto[] = productosDB.filter(producto => producto.categoria.toLowerCase() === categoria.toLowerCase());
+      if(productos.length > 0) {
+        resolve(productos);
+      }else {
+        reject(new Error(`Los productos con categoría ${categoria} no existen`))
+      }
+    },1000);
+  });
+  return promesa;
+}
